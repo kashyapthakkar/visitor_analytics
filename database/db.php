cfb6a2a6b15fa2469e66df86ce9c46a1292affe2
kashyapthakkar <47193812+kashyapthakkar@users.noreply.github.com>
2021-03-23 15:55:12 -0400

    new DB Added

--- database/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$DB['server'] = 'remotemysql.com';                                                //Server
-$DB['user'] = '6CkDUmmAHw';                                                       //username
-$DB['password'] = 'rptUj5wLe0';                                                       //password
-$DB['db'] = '6CkDUmmAHw';                                               //database name
--- process.php
+++ process.php
@@ -1,7 +1,9 @@
 <?php
 
-include "database/db.php";
-
+$DB['server'] = 'remotemysql.com';                                                //Server
+$DB['user'] = '6CkDUmmAHw';                                                       //username
+$DB['password'] = 'rptUj5wLe0';                                                       //password
+$DB['db'] = '6CkDUmmAHw';                                               //database name
 try
 {
 
